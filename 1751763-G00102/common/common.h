83658bc88166852de123bea23f9496eed8d5bd0a
wizardrobot1 <p1358197532@126.com>
2019-11-25 19:31:55 +0800

    common part

--- 1751763-G00102/common/common.h
+++ 1751763-G00102/common/common.h
@@ -1,4 +1,10 @@
-#define MAX_PKT 1024 //数据包长度，可按需改
+#ifndef __COMMON__H
+#define __COMMON__H
+#include "time.h"
+#include "stdio.h"
+#endif
+
+#define MAX_PKT 1024 //数据包长度
 typedef enum
 {
     false,
@@ -47,14 +53,12 @@ void from_network_layer(packet *p);
 void to_network_layer(packet *p);
 //接收方向网络层发送纯数据包
 //去掉帧的类型、发送/确认序号等控制信息
-//接收方向网络层发送纯数据包
-//去掉帧的类型、发送/确认序号等控制信息
+
+
 void from_physical_layer(packet *p);
 //接收方从物理层取得帧
 //帧头尾的FLAG字节、数据中的字节填充均已去掉
-//调用本函数前已验证过校验和，若发生错误
-//则发送cksum_err事件，因此只有帧正确的
-//情况下会调用本函数
+//调用本函数前已验证过校验和，若发生错误则发送cksum_err事件，因此只有帧正确的情况下会调用本函数
 
 void to_physical_layer(packet *p);
 //发送方向物理层发送帧
@@ -80,6 +84,23 @@ void disable_network_layer(void);
 //不再产生新的network_layer_ready事件
 //使网络层阻塞
 //不再产生新的network_layer_ready事件
+
+/*不同算法的MAX_SEQ不同，故不能宏定义
 #define inc(k) if(k<MAX_SEQ) k=k+1; else k=0;
 //使k在[0 ~ MAX_SEQ-1]间循环增长
 //如果MAX_SEQ=1，则0/1互换
+*/
+
+#define MYSIG_TIMEOUT SIGALARM//a) timeout 用 SIGALRM 信号，精度在 ms 级（不要用 alarm 函数）
+
+#define MYSIG_ACKTIMEOUT SIGALRM//b) ack_timeout 也用 SIGALRM 信号
+
+#define MYSIG_CHSUM_ERR 35//c) chsum_err 用自定义的 35 号信号
+
+#define MYSIG_FRAME_ARRIVAL 36//d) frame_arrival 用自定义的 36 号信号
+
+#define MYSIG_NETWORK_LAYER_READY 37//e) network_layer_ready 用自定义的 37 号信号
+
+#define MYSIG_ENABLE_NETWORK_LAYER 38//f) enable_network_layer 由 SDL 向 SNL 发送 38 号信号
+
+#define MYSIG_DISABLE_NETWORK_LAYER 39//g) disable_network_layer 由 SDL 向 SNL 发送 39 号信号
