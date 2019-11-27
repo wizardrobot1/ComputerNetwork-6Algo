1cf76b653cc8214fe5b41f4b1d26dff4b337b2c9
wizardrobot1 <p1358197532@126.com>
2019-11-27 13:28:14 +0800

    Update common.h
    void from_physical_layer(frame *f);

--- 1751763-G00102/common/common.h
+++ 1751763-G00102/common/common.h
@@ -65,9 +65,9 @@ void from_network_layer(packet *p,int pid);//发送方从网络层得到纯数据包
 
 void to_network_layer(packet *p,int pid);//接收方向网络层发送纯数据包,去掉帧的类型、发送/确认序号等控制信息
 
-void from_physical_layer(packet *p);//接收方从物理层取得帧,帧头尾的FLAG字节、数据中的字节填充均已去掉,调用本函数前已验证过校验和，若发生错误则发送cksum_err事件，因此只有帧正确的情况下会调用本函数
+void from_physical_layer(frame *f);//接收方从物理层取得帧,帧头尾的FLAG字节、数据中的字节填充均已去掉,调用本函数前已验证过校验和，若发生错误则发送cksum_err事件，因此只有帧正确的情况下会调用本函数
 
-void to_physical_layer(packet *p);//发送方向物理层发送帧,帧头尾加FLAG字节、数据中进行字节填充,计算校验和放入帧尾
+void to_physical_layer(frame *f);//发送方向物理层发送帧,帧头尾加FLAG字节、数据中进行字节填充,计算校验和放入帧尾
 
 
 void start_timer(seq_nr k);//启动第k帧的定时器
